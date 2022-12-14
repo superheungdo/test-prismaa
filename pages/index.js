2c8b7419d1e599026065a881ca974346a1f0e9f3
superbeom <it.super.beom@gmail.com>
2022-12-14 10:38:55 +0900

    Test

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,19 @@
 import firstPrismaClient from "../lib/firstClient";
 import secondPrismaClient from "../lib/secondClient";
 
-const Home = () => {
+const Home = ({ device, admin }) => {
   return (
     <div className="bg-red-300 w-full h-screen grid grid-cols-16">
-      <div className="bg-green-200">1</div>
-      <div className="bg-blue-200">2</div>
+      <div className="bg-green-200">
+        {device.map((item) => (
+          <div key={item.id}>name: {item.name}</div>
+        ))}
+      </div>
+      <div className="bg-blue-200">
+        {admin.map((item) => (
+          <div key={item.id}>name: {item.name}</div>
+        ))}
+      </div>
       <div className="bg-orange-200">3</div>
     </div>
   );
@@ -14,13 +22,16 @@ const Home = () => {
 export default Home;
 
 export const getServerSideProps = async () => {
-  const first = await firstPrismaClient.device.findMany();
-  const second = await secondPrismaClient.admin.findMany();
+  const device = await firstPrismaClient.device.findMany();
+  const admin = await secondPrismaClient.admin.findMany();
 
-  console.log("first: ", first);
-  console.log("second: ", second);
+  console.log("device: ", device);
+  console.log("admin: ", admin);
 
   return {
-    props: {},
+    props: {
+      device,
+      admin,
+    },
   };
 };
